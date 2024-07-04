92aabe5f62636496c3a0be951aa4734ad75a849a
sapirschiff <159164447+sapirschiff@users.noreply.github.com>
2024-07-04 20:15:02 +0300

    Update AdvancementCard.cpp

--- AdvancementCard.cpp
+++ AdvancementCard.cpp
@@ -1,8 +1,1 @@
-#include "AdvancementCard.hpp"
 
-AdvancementCard::AdvancementCard(ResourceType resourceType)
-    : Card("Advancement"), resourceType(resourceType) { }
-
-AdvancementCard::ResourceType AdvancementCard::getResourceType() const {
-    return resourceType;
-}
