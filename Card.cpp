50b426e16120c77b781b8aa158ff96ab30a55d01
sapirschiff <159164447+sapirschiff@users.noreply.github.com>
2024-07-06 20:59:22 +0300

    Delete Card.cpp

--- Card.cpp
+++
@@ -1,9 +0,0 @@
-#include "Card.hpp"
-
-// Constructor to initialize the card with a specific type
-Card::Card(const std::string& type) : type(type) { }
-
-// Method to get the type of the card
-std::string Card::getType() const {
-    return type;
-}
